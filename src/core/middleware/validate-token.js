3dad3a7339fd3b588dfc1f205a8b9381c2af62f6
Hafiz Ahmed <rhafizubd@gmail.com>
2021-01-14 01:55:00 +0600

    handled promise in validate token

--- src/core/middleware/validate-token.js
+++ src/core/middleware/validate-token.js
@@ -4,12 +4,17 @@ const CustomError = require('../custom-error');
 validateToken = async function(req, res, next) {
   let token = req.headers.authorization;
   token = token.replace('Bearer ', '');
-  const result = await check_token(token);
-  if(result.statusCode == 200) {
-    next();
-  } else {
-    next(new CustomError(result.statusCode, result.statusMessage));
-  }
+  check_token(token).then(
+    result => {
+      if(result.statusCode == 200) {
+        next();
+      } else {
+        next(new CustomError(result.statusCode, result.statusMessage));
+      }
+    }
+  ).catch(
+    error => next(new CustomError(500, error))
+  );
 }
 
 module.exports = validateToken;
